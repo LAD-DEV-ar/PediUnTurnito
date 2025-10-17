1e0ceddae39b8a8adbcf0cc8742772a918b4d05e
LAD-DEV-ar <eliasaarroyo@gmail.com>
2025-10-17 19:53:53 -0300

    v0.0.3 Agragando funciones principales

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -6,7 +6,7 @@ use MVC\Router;
 
 class DashboardController{
     public static function index(Router $router){
-        
+        start_session_if_needed();
 
         $router->render('dashboard/index', []);
     }
--- includes/funciones.php
+++ includes/funciones.php
@@ -1,10 +1,33 @@
 <?php
 
-function debuguear($variable) : string {
+function debug($variable) : string {
     echo "<pre>";
     var_dump($variable);
     echo "</pre>";
     exit;
+}
+
+function debugVarios($debugs = []) : array {
+    foreach($debugs as $i){
+        echo "<pre>";
+        var_dump($variable);
+        echo "</pre>";
+    }
+    exit;
+}
+
+function start_session_if_needed(): void {
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
+}
+
+function require_login_simple(string $redirect = '/'): void {
+    start_session_if_needed();
+    if (empty($_SESSION['id']) || empty($_SESSION['login'])) {
+        header('Location: ' . $redirect);
+        exit;
+    }
 }
 
 // Escapa / Sanitizar el HTML
